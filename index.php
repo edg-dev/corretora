90ebeea05243350290199961a1a45326a1e930bb
Wender Lucas <Miyukii@users.noreply.github.com>
2019-04-19 17:47:43 -0300

    Cadastro de pessoa física.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php include '1.View/Templates/header.php'; ?>
+<?php include 'View/Templates/header.php'; ?>
 
 <p>Hello world!</p>
 
-<?php include "1.View/Templates/footer.php"; ?>
+<?php include "View/Templates/footer.php"; ?>
