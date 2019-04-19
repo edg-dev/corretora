93ee08a8475a5ff5c18089792a993f74d516999e
Wender Lucas <Miyukii@users.noreply.github.com>
2019-04-19 15:43:28 -0300

    Templates (header e footer), jquery e bootstrap

---
+++ 1.View/Templates/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+
+        <title>Título teste</title>
+
+        <!-- CSS  -->
+        <link href="/Projeto-Integrador-V/Config/Bootstrap/bootstrap.css" type="text/css" rel="stylesheet">
+
+        <!-- JavaScript  -->
+        <script src="/Projeto-Integrador-V/Config/JS/jquery-3.2.1.min.js"></script>
+        <script src="/Projeto-Integrador-V/Config/JS/bootstrap.js"></script>
+        <script src="/Projeto-Integrador-V/Config/JS/init.js"></script>
+
+
+    </head>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include '1.View/Templates/header.php'; ?>
+
+<p>Hello world!</p>
+
+<?php include "1.View/Templates/footer.php"; ?>
