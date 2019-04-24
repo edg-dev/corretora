fd754c06c12917e2bc86eecc4d68a281fa8cd564
Leonardo Braga <leonardobraga24@gmail.com>
2019-04-24 20:01:25 -0300

    Update Imovel.php
    Adicionado as opções de estado no cadastro do imóvel.

--- View/Cadastro/Imovel.php
+++ View/Cadastro/Imovel.php
@@ -51,7 +51,36 @@
     <div class="form-group col-md-6">
         <label for="cep"><span>*</span>Cep:</label>
         <input type="text" class="form-control" id="cep" placeholder="00000-000">
-        <input type="text" class="form-control" id="estado" placeholder="Estado">
+		<select class="form-control" id="uf">
+            <option selected>Estado</option>
+			<option value="AC">Acre</option>
+			<option value="AL">Alagoas</option>
+			<option value="AP">Amapá</option>
+			<option value="AM">Amazonas</option>
+			<option value="BA">Bahia</option>
+			<option value="CE">Ceará</option>
+			<option value="DF">Distrito Federal</option>
+			<option value="ES">Espírito Santo</option>
+			<option value="GO">Goiás</option>
+			<option value="MA">Maranhão</option>
+			<option value="MT">Mato Grosso</option>
+			<option value="MS">Mato Grosso do Sul</option>
+			<option value="MG">Minas Gerais</option>
+			<option value="PA">Pará</option>
+			<option value="PB">Paraíba</option>
+			<option value="PR">Paraná</option>
+			<option value="PE">Pernambuco</option>
+			<option value="PI">Piauí</option>
+			<option value="RJ">Rio de Janeiro</option>
+			<option value="RN">Rio Grande do Norte</option>
+			<option value="RS">Rio Grande do Sul</option>
+			<option value="RO">Rondônia</option>
+			<option value="RR">Roraima</option>
+			<option value="SC">Santa Catarina</option>
+			<option value="SP">São Paulo</option>
+			<option value="SE">Sergipe</option>
+			<option value="TO">Tocantins</option>
+		</select>
         <input type="text" class="form-control" id="cidade" placeholder="Cidade">
     </div>
     <div class="form-group col-md-6">
@@ -100,7 +129,7 @@
         <label for="precoImovel">Valor total de venda (R$):</label>
         <input type="text" class="form-control" id="precoImovel" placeholder="000 000">
     </div>
-    
+
     <div class="form-group col-md-6">
         <button type="submit" class="btn btn-primary">Salvar</button>
     </div>
