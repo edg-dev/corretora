9d0d0a39e83a3f282d3c43ec58d4b60b10c36914
Alex Júnio <31208985+alexjuniogoncalves17001391@users.noreply.github.com>
2019-05-01 17:27:18 -0300

    login

--- View/login/user/login-user.php
+++ View/login/user/login-user.php
@@ -7,13 +7,13 @@
   <div class="login-box-body">
     <p class="login-box-msg">Faça o Login para iniciar a sessão</p>
 
-    <form action="../../../index.html" method="post">
+    <form action="../../../index.php" method="post">
       <div class="form-group has-feedback">
-        <input type="text" class="form-control" placeholder="Login" name="deslogin">
+        <input value="<?php if(isset($_SESSION['email'])) echo $_SESSION['email']; ?>" type="text" class="form-control" placeholder="Email" >
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password" name="despassword">
+        <input type="password" class="form-control" placeholder="Senha" >
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
@@ -26,7 +26,7 @@
         </div>
 
         <div class="form-group col-md-4">
-          <button type="submit" href="index.html" class="btn btn-primary btn-block btn-flat">Logar</button>
+          <button type="submit" href="index.php" class="btn btn-primary btn-block btn-flat">Logar</button>
         </div>
 
         <div class="col-md-4">
@@ -52,5 +52,51 @@
   </div>
 
 </div>
+<?php
+if(!isset($_SESSION))
+    session_start();
+
+//Login de Usários
+if(isset($_POST[login])){
+
+  include('../conexao.php');
+  
+  $erro = array();
+
+  // Captação de dados
+    $senha = $_POST[password];
+    $_SESSION['email'] = $mysqli->escape_string($_POST['email']);
+
+    // Validação de dados
+    if(!filter_var($_SESSION['email'], FILTER_VALIDATE_EMAIL))
+        $erro[] = "Preencha seu <strong>e-mail</strong> corretamente.";
+
+    if(strlen($senha) < 6 || strlen($senha) > 16)
+        $erro[] = "Preencha sua <strong>senha</strong> corretamente.";
+
+    if(count($erro) == 0){
+
+        $sql = "SELECT senha as senha, idUsuario as valor 
+        FROM Usuario 
+        WHERE emailLogin = '$_SESSION[email]'";
+        $que = $mysqli->query($sql) or die($mysqli->error);
+        $dado = $que->fetch_assoc();
+        
+        if($que->num_rows == 0)
+            $erro[] = "Nenhum usuário possui o <strong>e-mail</strong> informado.";
+
+        elseif(strcmp($dado[senha], ($senha)) == 0){
+            $_SESSION[Usuario_Logado] = $dado[valor];
+        }else
+            $erro[] = "<strong>Senha</strong> incorreta.";
+
+        if(count($erro) == 0){
+            echo "<script>location.href='index.php=';</script>";
+            exit();
+            unset($_SESSION['email']);
+        }
+    }
+}
+?>
 
 <?php include '../../Templates/footer.php'; ?>
