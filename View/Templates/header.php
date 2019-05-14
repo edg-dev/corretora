5918cb7b45ccb2962350a51816570d5d7662503c
Leonardo Braga <leonardobraga24@gmail.com>
2019-05-14 15:17:29 -0300

    Fazendo alguns testes de templates
    Possiivelmente não será possivel a mesclação de templates.

--- View/Templates/header.php
+++ View/Templates/header.php
@@ -8,6 +8,7 @@
         <title>Corretora</title>
 
         <!-- CSS  -->
+        <link href="/corretora/Config/CSS/style.css" type="text/css" rel="stylesheet">
         <link href="/corretora/Config/Bootstrap/bootstrap.css" type="text/css" rel="stylesheet">
 
         <!-- JavaScript  -->
@@ -18,15 +19,15 @@
     </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="/corretora/index.php">(Logo)</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active" >
                     <a class="nav-link" href="/corretora/index.php">Inicio<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
