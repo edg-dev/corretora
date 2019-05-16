0cc0b901deaa76607daf8ada0d0c7fb60d3e323d
Leonardo Braga <leonardobraga24@gmail.com>
2019-05-16 15:14:32 -0300

    Adicionado o foreach na index

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php include 'View/Templates/header.php'; ?>
+<?php include 'View/Templates/header.php'; 
 
+    require_once $_SERVER["DOCUMENT_ROOT"] . "/corretora/Model/ImovelModel.php";
+    
+    $imovelModel = new ImovelModel();
+    $imoveis = $imovelModel->getAllImovel();
+
+?>
     <h1>NOME DO SITE</h1>
 
     <div class="bd-example">
@@ -46,11 +52,12 @@
                     <div class="container">
 
                 <!-- Page Heading -->
-                <h1 class="my-4">Page Heading
-                <small>Secondary Text</small>
+                <h1 class="my-4">Anúncios
+                
                 </h1>
 
                 <!-- Project One -->
+                <?php foreach($imoveis as $imovel){?>
                 <div class="row">
                 <div class="col-md-7">
                     <a href="#">
@@ -58,68 +65,29 @@
                     </a>
                 </div>
                 <div class="col-md-5">
-                    <h3>Project One</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-                    <a class="btn btn-primary" href="#">View Project</a>
+                    <p> 
+                        <?php echo $imovel['descricaoTransacao'];?>
+                        <?php echo $imovel['descricaoTipoImovel'];?>
+                        <?php echo $imovel['precoImovel'];?>
+                        <?php echo $imovel['areaUtil'];?>
+                        <?php echo $imovel['areaTotal'];?>
+                        <?php echo $imovel['quantQuarto'];?>
+                        <?php echo $imovel['quantSuite'];?>
+                        <?php echo $imovel['quantVagaGaragem'];?>
+                        <?php echo $imovel['quantBanheiro'];?>
+                        <?php echo $imovel['descricaoImovel'];?>
+
+                    </p>                
+                    <a class="btn btn-primary" href="#">Detalhes</a>
+  
                 </div>
                 </div>
                 <!-- /.row -->
-
                 <hr>
-
-                <!-- Project Two -->
-                <div class="row">
-                <div class="col-md-7">
-                    <a href="#">
-                    <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
-                    </a>
-                </div>
-                <div class="col-md-5">
-                    <h3>Project Two</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.</p>
-                    <a class="btn btn-primary" href="#">View Project</a>
-                </div>
-                </div>
-                <!-- /.row -->
-
-                <hr>
-
-                <!-- Project Three -->
-                <div class="row">
-                <div class="col-md-7">
-                    <a href="#">
-                    <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
-                    </a>
-                </div>
-                <div class="col-md-5">
-                    <h3>Project Three</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!</p>
-                    <a class="btn btn-primary" href="#">View Project</a>
-                </div>
-                </div>
-                <!-- /.row -->
-
-                <hr>
-
-                <!-- Project Four -->
-                <div class="row">
-
-                <div class="col-md-7">
-                    <a href="#">
-                    <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
-                    </a>
-                </div>
-                <div class="col-md-5">
-                    <h3>Project Four</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem officiis illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>
-                    <a class="btn btn-primary" href="#">View Project</a>
-                </div>
-                </div>
-                <!-- /.row -->
-
-                <hr>
+                <?php } ?>
 
                 <!-- Pagination -->
+
                 <ul class="pagination justify-content-center">
                 <li class="page-item">
                     <a class="page-link" href="#" aria-label="Previous">
