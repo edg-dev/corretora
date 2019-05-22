fcbfe9036af01ce48e6a5d6c93a8919c88cd1d8a
Alex Júnio <31208985+alexjuniogoncalves17001391@users.noreply.github.com>
2019-05-22 20:19:42 -0300

    bao

--- View/login/user/conexao.php
+++ View/login/user/conexao.php
@@ -1,11 +1,13 @@
 <?php
 
-define("host" ,"localhost");
-define("usuario" , "root");
-define("senha" , "");
-define("bd" , "corretora");
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$bd = "corretora";
 
-$conexao = mysqli_connect(host, usuario, senha, bd) or die('Não foi possivel fazer Login');
+$mysqli = new mysqli($host, $usuario, $senha, $bd);
 
+if($mysqli->connect_errno)
+  echo "Falha na conexão: (".$mysqli->connect_errno.") ".$mysqli->connect_error;
 
 ?>
