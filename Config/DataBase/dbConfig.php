335cc3755e00fd4b1335fed9c0d67e9347f6e3cf
Alex Júnio <31208985+alexjuniogoncalves17001391@users.noreply.github.com>
2019-05-22 13:16:41 -0300

    peguei sua branch sem quere ai tenho que dar o comite

--- Config/DataBase/dbConfig.php
+++ Config/DataBase/dbConfig.php
@@ -6,7 +6,7 @@
 		public static function obterConexao(){
 			if(isset($conexao) == false){
 
-				$conexao = new PDO("mysql:dbname=corretora;host=localhost","root","root");
+				$conexao = new PDO("mysql:dbname=corretora;host=localhost","root","");
 
 				$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}
