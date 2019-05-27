7b42464029edba61d04c641f40e4b1bc3dba80bd
Wender Lucas <Miyukii@users.noreply.github.com>
2019-05-26 22:20:31 -0300

    Retorno de Imagens na página de anúncio

--- View/Pages/anuncio.php
+++ View/Pages/anuncio.php
@@ -2,6 +2,7 @@
 
     require_once $_SERVER["DOCUMENT_ROOT"] . "/corretora/Model/ImovelModel.php";
     require_once $_SERVER["DOCUMENT_ROOT"] . "/corretora/Model/TelefoneModel.php";
+    require_once $_SERVER["DOCUMENT_ROOT"] . "/corretora/Model/ImagensImovelModel.php";
 
     $idImovel = $_GET["id"];
 
@@ -13,7 +14,46 @@
     $TelefoneModel = new TelefoneModel();
     $telefones = $TelefoneModel->getTelefonesById($idPessoa);
 
+    $ImagemModel = new ImagensImovelModel();
+    $imagens = $ImagemModel->listarArrayImagens($idImovel);
+
 ?>
+<style>
+.gallery {
+    -webkit-column-count: 3;
+    -moz-column-count: 3;
+    column-count: 3;
+    -webkit-column-width: 33%;
+    -moz-column-width: 33%;
+    column-width: 33%; }
+    .gallery .pics {
+    -webkit-transition: all 350ms ease;
+    transition: all 350ms ease; }
+    .gallery .animation {
+    -webkit-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1); 
+}
+
+@media (max-width: 450px) {
+    .gallery {
+    -webkit-column-count: 1;
+    -moz-column-count: 1;
+    column-count: 1;
+    -webkit-column-width: 100%;
+    -moz-column-width: 100%;
+    column-width: 100%;
+    }
+}
+
+@media (max-width: 400px) {
+    .btn.filter {
+    padding-left: 1.1rem;
+    padding-right: 1.1rem;
+    }
+}
+</style>
+
     <div class="container">
         <h1>Detalhes do anúncio:</h1>
         <br>
@@ -107,6 +147,28 @@
             <h4>Imagens do imóvel:</h4>
         </form>
 
-    </div>
+        <div class="gallery" id="gallery">
+        <?php foreach($imagens as $imagem) { ?>
+            <div class="mb-3">
+                <img class="img-fluid" src="/corretora/Files/<?php echo $imagem?>">
+            </div>
+            <?php } ?>
+        </div>
 
+    </div>
+<script>
+    $(function() {
+        var selectedClass = "";
+
+        $(".filter").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#gallery").fadeTo(100, 0.1);
+            $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+            setTimeout(function() {
+                $("."+selectedClass).fadeIn().addClass('animation');
+                $("#gallery").fadeTo(300, 1);
+            }, 300);
+        });
+    });
+</script>
 <?php include "../Templates/footer.php"; ?>
