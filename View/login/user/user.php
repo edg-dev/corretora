65067bd5b0b3fe578399d126d1b657ba44ed9292
Alex Júnio <31208985+alexjuniogoncalves17001391@users.noreply.github.com>
2019-05-27 22:59:28 -0300

    user 1.0

--- View/login/user/user.php
+++ View/login/user/user.php
@@ -1,4 +1,5 @@
 <?php include '../../Templates/header.php'?>
+
 <?php
 
 include('verifica_login.php');
@@ -13,7 +14,7 @@ include('verifica_login.php');
 <!------ Include the above in your HEAD tag ---------->
 
 <head>
-  <title>Bootstrap Example</title>
+  <title>Usuário</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -26,7 +27,13 @@ include('verifica_login.php');
 <div class="container bootstrap snippet">
     <div class="row">
   		<div class="col-sm-10"><h1>Olá, <?php echo $_SESSION['usuario'];?></h1></div>
-    	<div class="col-sm-2"><a href="\corretora\View\login\user\logout.php" class="pull-right"><img title="profile image" class="img-circle img-responsive" src="http://www.gravatar.com/avatar/28fd20ccec6865e2d5f0e1f4446eb7bf?s=100"></a></div>
+    	<div class="col-sm-2"><a href="\corretora\View\login\user\logout.php" class="pull-right">
+        <img title="Fazer Logout" class="img-circle img-responsive" 
+        src="http://www.gravatar.com/avatar/28fd20ccec6865e2d5f0e1f4446eb7bf?s=100"></a>
+        <button class="btn btn-lg btn-success" type="submit"><i class="glyphicon glyphicon-ok-sign">
+        <a href="\corretora\View\login\user\logout.php" class="pull-right"></i> Sair</a></button>
+        </div>
+        
     </div>
     <div class="row">
   		<div class="col-sm-3"><!--left col-->
@@ -41,7 +48,7 @@ include('verifica_login.php');
           
           
           <ul class="list-group">
-            <li class="list-group-item text-muted">Geral<i class="fa fa-dashboard fa-1x"></i></li>
+            <li class="list-group-item text-muted">Geral</li>
             <li class="list-group-item text-right"><span class="pull-left"><strong>Total de Anuncios</strong></span> 5</li>
             <li class="list-group-item text-right"><span class="pull-left"><strong>Mensagens</strong></span> 99</li>
             
@@ -71,7 +78,7 @@ include('verifica_login.php');
                       <div class="form-group">
                           
                           <div class="col-xs-6">
-                            <label for="last_name"><h4>Last name</h4></label>
+                            <label for="last_name"><h4>Segundo nome</h4></label>
                               <input type="text" class="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any.">
                           </div>
                       </div>
@@ -96,10 +103,7 @@ include('verifica_login.php');
                       </div>
                       <div class="form-group">
                           
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Location</h4></label>
-                              <input type="email" class="form-control" id="location" placeholder="somewhere" title="enter a location">
-                          </div>
+                         
                       </div>
                       <div class="form-group">
                           
@@ -138,72 +142,7 @@ include('verifica_login.php');
             		
                	
                   <hr>
-                  <form class="form" action="##" method="post" id="registrationForm">
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="first_name"><h4>First name</h4></label>
-                              <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                            <label for="last_name"><h4>Last name</h4></label>
-                              <input type="text" class="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any.">
-                          </div>
-                      </div>
-          
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="phone"><h4>Phone</h4></label>
-                              <input type="text" class="form-control" name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any.">
-                          </div>
-                      </div>
-          
-                      <div class="form-group">
-                          <div class="col-xs-6">
-                             <label for="mobile"><h4>Mobile</h4></label>
-                              <input type="text" class="form-control" name="mobile" id="mobile" placeholder="enter mobile number" title="enter your mobile number if any.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Email</h4></label>
-                              <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Location</h4></label>
-                              <input type="email" class="form-control" id="location" placeholder="somewhere" title="enter a location">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="password"><h4>Password</h4></label>
-                              <input type="password" class="form-control" name="password" id="password" placeholder="password" title="enter your password.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                            <label for="password2"><h4>Verify</h4></label>
-                              <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                           <div class="col-xs-12">
-                                <br>
-                              	<button class="btn btn-lg btn-success pull-right" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
-                               	<!--<button class="btn btn-lg" type="reset"><i class="glyphicon glyphicon-repeat"></i> Reset</button>-->
-                            </div>
-                      </div>
-              	</form>
+                  
               </div>
                
               </div><!--/tab-pane-->
