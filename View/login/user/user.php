f7d0965f102d0fed7155d618fe7bee702a597c6d
Alex Júnio <31208985+alexjuniogoncalves17001391@users.noreply.github.com>
2019-05-27 22:16:18 -0300

    user

--- View/login/user/user.php
+++ View/login/user/user.php
@@ -42,24 +42,18 @@ include('verifica_login.php');
           
           <ul class="list-group">
             <li class="list-group-item text-muted">Geral<i class="fa fa-dashboard fa-1x"></i></li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Total de Anuncios</strong></span> 125</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Mensagens</strong></span> 13</li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong>Total de Anuncios</strong></span> 5</li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong>Mensagens</strong></span> 99</li>
             
           </ul> 
                
-          <div class="panel panel-default">
-            <div class="panel-heading">Social Media</div>
-            <div class="panel-body">
-            	<i class="fa fa-facebook fa-2x"></i> <i class="fa fa-github fa-2x"></i> <i class="fa fa-twitter fa-2x"></i> <i class="fa fa-pinterest fa-2x"></i> <i class="fa fa-google-plus fa-2x"></i>
-            </div>
-          </div>
-          
+                    
         </div><!--/col-3-->
     	<div class="col-sm-9">
             <ul class="nav nav-tabs">
                 <li class="active"><a data-toggle="tab" href="#home">Pessoal</a></li>
                 <li><a data-toggle="tab" href="#messages">Anuncios</a></li>
-                <li><a data-toggle="tab" href="#settings">Menu 2</a></li>
+                
               </ul>
 
               
@@ -137,73 +131,7 @@ include('verifica_login.php');
                
                <h2></h2>
                
-               <hr>
-                  <form class="form" action="##" method="post" id="registrationForm">
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="first_name"><h4>First name</h4></label>
-                              <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                            <label for="last_name"><h4>Last name</h4></label>
-                              <input type="text" class="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any.">
-                          </div>
-                      </div>
-          
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="phone"><h4>Phone</h4></label>
-                              <input type="text" class="form-control" name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any.">
-                          </div>
-                      </div>
-          
-                      <div class="form-group">
-                          <div class="col-xs-6">
-                             <label for="mobile"><h4>Mobile</h4></label>
-                              <input type="text" class="form-control" name="mobile" id="mobile" placeholder="enter mobile number" title="enter your mobile number if any.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Email</h4></label>
-                              <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Location</h4></label>
-                              <input type="email" class="form-control" id="location" placeholder="somewhere" title="enter a location">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="password"><h4>Password</h4></label>
-                              <input type="password" class="form-control" name="password" id="password" placeholder="password" title="enter your password.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                            <label for="password2"><h4>Verify</h4></label>
-                              <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                           <div class="col-xs-12">
-                                <br>
-                              	<button class="btn btn-lg btn-success" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
-                               	<button class="btn btn-lg" type="reset"><i class="glyphicon glyphicon-repeat"></i> Reset</button>
-                            </div>
-                      </div>
-              	</form>
+               //
                
              </div><!--/tab-pane-->
              <div class="tab-pane" id="settings">
@@ -284,5 +212,3 @@ include('verifica_login.php');
         </div><!--/col-9-->
     </div><!--/row-->
 
-<h2>Olá, <?php echo $_SESSION['usuario'];?></h2>
-<h2><a href="logout.php">Sair</a></h2>
