c02db85c707cf8bdc03b15e7ce25870ac8ede4d2
Gustavo <31208960+RangerBK@users.noreply.github.com>
2019-06-04 00:21:40 -0300

    Arrumando o Login,Index e colocando o Documento do Cartão do Grupo

--- View/login/user/index.php
+++ View/login/user/index.php
@@ -1,56 +1,41 @@
 <?php include '../../Templates/header.php'?>
 
-
-<!DOCTYPE html>
-<html>
-    
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <link rel="stylesheet" href="css/bulma.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/login.css">
-</head>
-
 <body>
-    <section class="hero is-success is-fullheight">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Entre aqui!</h3>
+
+    <h2>Entre aqui!</h2>
                     
-                    <?php
-                    if(isset($_SESSION['nao_autenticado'])):
-                    ?>
-                    <div class="notification is-danger">
-                      <p>ERRO: Usuário ou senha inválidos.</p>
-                    </div>
-                    <?php
-                    endif;
-                    unset($_SESSION['nao_autenticado']);
-                    ?>
-                    <div class="box">
-                        <form action="login.php" method="POST">
-                            <div class="field">
-                                <div class="control">
-                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
-                                </div>
-                            </div>
-
-                            <div class="field">
-                                <div class="control">
-                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
-                                </div>
-                            </div>
-                            <button type="submit"  class="btn btn-primary"><i class="fas fa-fw fa-user"></i> Entrar</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+    <?php
+        if(isset($_SESSION['nao_autenticado'])):
+    ?>
+    <div class="notification is-danger">
+        <p>ERRO: Usuário ou senha inválidos.</p>
+    </div>
+    <?php
+        endif;
+        unset($_SESSION['nao_autenticado']);
+    ?>
+    <form action="login.php" method="POST">
+        <div class="imgcontainer">
+            <img src="../../../Files/img_avatar2.png" alt="Avatar" class="avatar">
         </div>
-    </section>
+        <div class="container">
+            <label for="usuario"><b>Usuario</b></label>
+            <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
+                                
+            <label for="senha"><b>Senha</b></label>
+            <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
+ 
+            <button type="submit"  class="btn btn-primary">Entrar</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Lembrar-Me
+            </label>
+        </div>
+        <!--
+        <div class="container" style="background-color:#f1f1f1">
+            <span class="psw">Esqueceu a <a href="#">Senha?</a></span>
+        </div>
+        -->
+    </form>
+     
 </body>
-
 </html>
