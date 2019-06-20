b455e2ff22b5a1499efa28c88d9bc66b9ff6f1f5
Wender Lucas <Miyukii@users.noreply.github.com>
2019-06-20 19:51:44 -0300

    Correções de conexão com o banco

--- Config/DataBase/dbConfig.php
+++ Config/DataBase/dbConfig.php
@@ -5,10 +5,10 @@
 
 		public static function obterConexao(){
 			if(isset($conexao) == false){
-				$conexao = new PDO("mysql:dbname=id9949090_corretora;
+				$conexao = new PDO("mysql:dbname=corretora;
                     				host=localhost;charset=utf8",
-                    				"id9949090_corretora",
-                    				"ggcorretorapi5", 
+                    				"root",
+                    				"", 
                     				array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 				$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}
